3894b7e2bb19af620eee379d42f4f10fc4357ddc
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-05-26 11:20:41 +0700

    Membuat

--- Doublelinkedlist_!60/Doublelinkedlist_!60.cpp
+++ Doublelinkedlist_!60/Doublelinkedlist_!60.cpp
@@ -25,6 +25,36 @@ public:
     void searchData();
 };
 
+DoubleLinkedList::DoubleLinkedList() {
+    START = NULL;
+}
+
+void DoubleLinkedList::addNode() {
+    int nim;
+    string nm;
+    cout << "\nEntar the roll number o f the student: ";
+    cin >> nim;
+    cout << "\nEnter the name of the student: ";
+    cin >> nm;
+    Node* newNode = new Node();             //Step 1
+    newNode->noMhs = nim;                   //Step 2
+    newNode->name = nm;                     //Step 2
+
+    /*Insert a node on the beginning of a double linked list*/
+    if (START == NULL ||nim <= START -> noMhs)   {
+        if (START != NULL && nim == START -> noMhs)
+        {
+            cout << "\nDuplicate number not allowed" << endl;
+            return;
+        }
+        newNode->next = START;		//Step 3 
+        if (START != NULL)
+            START->prev = newNode;  //Step 4
+        newNode->prev = NULL;       //Step 5
+        START = newNode;            //Step 6
+        return;
+    }
+}
 
 
 
